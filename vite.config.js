1a14d621cc2157f34a13dbdf1fb9bee0bb9b4de5
cholocate-taffy <az2604006360@gmail.com>
2025-09-02 09:30:14 +0800

    Fix build configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/taffy/', 
-})
+    base: '/taffy/',
+    build: {
+        chunkSizeWarningLimit: 1600, 
+    }
+});
